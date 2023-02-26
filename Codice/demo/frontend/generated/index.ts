682c9d182bf5a673faf46649e13cf00fb1398a58
Benjaminos1998 <beniamino.infante.98@gmail.com>
2023-02-26 16:35:54 +0100

    Modifiche codice
    aggiunta vaadin

--- Codice/demo/frontend/generated/index.ts
+++ Codice/demo/frontend/generated/index.ts
@@ -15,18 +15,18 @@ import { Router } from '@vaadin/router';
 
 // import Flow module to enable navigation to Vaadin server-side views
 import { Flow } from 'Frontend/generated/jar-resources/Flow.js';
-//
-//const { serverSideRoutes } = new Flow({
-//  imports: () => import('../../target/frontend/generated-flow-imports.js')
-//});
 
-//const routes = [
-//  // for client-side, place routes below (more info https://vaadin.com/docs/v15/flow/typescript/creating-routes.html)
-//
-//  // for server-side, the next magic line sends all unmatched routes:
-//  ...serverSideRoutes // IMPORTANT: this must be the last entry in the array
-//];
-//
-//// Vaadin router needs an outlet in the index.html page to display views
-//const router = new Router(document.querySelector('#outlet'));
-//router.setRoutes(routes);
+const { serverSideRoutes } = new Flow({
+  imports: () => import('../../target/frontend/generated-flow-imports.js')
+});
+
+const routes = [
+  // for client-side, place routes below (more info https://vaadin.com/docs/v15/flow/typescript/creating-routes.html)
+
+  // for server-side, the next magic line sends all unmatched routes:
+  ...serverSideRoutes // IMPORTANT: this must be the last entry in the array
+];
+
+// Vaadin router needs an outlet in the index.html page to display views
+const router = new Router(document.querySelector('#outlet'));
+router.setRoutes(routes);
--- Codice/demo/src/main/java/noleggioAuto/DemoApplication.java
+++ Codice/demo/src/main/java/noleggioAuto/DemoApplication.java
@@ -3,8 +3,6 @@ package noleggioAuto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-
 @SpringBootApplication
 public class DemoApplication {
 
