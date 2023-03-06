9eebb8a172970a3186dd867362a2e341e17a9cc7
Benjaminos1998 <beniamino.infante.98@gmail.com>
2023-03-06 16:15:36 +0100

    modifiche codice

--- Codice/demo/src/main/java/noleggioAuto/entities/Business.java
+++ Codice/demo/src/main/java/noleggioAuto/entities/Business.java
@@ -1,17 +1,15 @@
-//package noleggioAuto.entities;
-//
-//
-//public class Business extends Auto {
-//
-//	private static final Integer puntiBonus=100;
-//
-//	// COSTRUTTORI
-//	
-//	
-//	public Business(String targa, String modello) {
-//		super(targa, modello);
-//	}
-//	
-//	
-//
-//}
+package noleggioAuto.entities;
+
+
+public class Business extends Auto {
+
+	private static final Integer puntiBonus=100;
+
+	// COSTRUTTORI
+	
+	
+	public Business(String targa, String modello) {
+		super(targa, modello);
+	}
+	
+}
--- Codice/demo/src/main/java/noleggioAuto/entities/Utilitaria.java
+++ Codice/demo/src/main/java/noleggioAuto/entities/Utilitaria.java
@@ -1,24 +1,24 @@
-//package noleggioAuto.entities;
-//
-//
-//public class Utilitaria extends Auto {
-//
-//	// CAMPI
-//
-//	private static final Integer puntiBonus = 50;
-//	private String prezzo;
-//
-//	// COSTRUTTORI
-//	
-//
-//	public Utilitaria(String targa, String modello) {
-//		super(targa, modello);
-//	}
-//	
-//	public Utilitaria(String targa,String modello,String prezzo) {
-//		this(targa, modello);
-//		this.prezzo=prezzo;
-//	}
-//	
-//
-//}
+package noleggioAuto.entities;
+
+
+public class Utilitaria extends Auto {
+
+	// CAMPI
+
+	private static final Integer puntiBonus = 50;
+	private String prezzo;
+
+	// COSTRUTTORI
+	
+
+	public Utilitaria(String targa, String modello) {
+		super(targa, modello);
+	}
+	
+	public Utilitaria(String targa,String modello,String prezzo) {
+		this(targa, modello);
+		this.prezzo=prezzo;
+	}
+	
+
+}
