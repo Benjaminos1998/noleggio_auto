4cbfe3d9ee3be8294564327111fa9f63463d7210
Benjaminos1998 <beniamino.infante.98@gmail.com>
2023-03-15 21:39:00 +0100

    frontendNoleggioAuto
    Creazione progetto Angular con node.js

---
+++ Codice/backendNoleggioAuto/src/main/java/noleggioAuto/NoleggioAuto.java
@@ -0,0 +1,12 @@
+package noleggioAuto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoleggioAuto {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoleggioAuto.class, args);
+	}
+}
