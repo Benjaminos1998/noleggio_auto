7c3d7f95604f2a7087ae729593466e3739891669
Benjaminos1998 <beniamino.infante.98@gmail.com>
2023-03-15 21:52:02 +0100

    frontendNoleggioAuto
    Creazione dei componenti registrazione, login e dashboard

--- Codice/frontendNoleggioAuto/src/app/app-routing.module.ts
+++ Codice/frontendNoleggioAuto/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './componenti/dashboard/dashboard.component';
+import { LoginComponent } from './componenti/login/login.component';
+import { RegistrazioneComponent } from './componenti/registrazione/registrazione.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:DashboardComponent},
+  {path:'login',component:LoginComponent},
+  {path:'registrazione',component:RegistrazioneComponent},
+  {path:'dashboard',component: DashboardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Codice/frontendNoleggioAuto/src/app/app.module.ts
+++ Codice/frontendNoleggioAuto/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './componenti/dashboard/dashboard.component';
+import { LoginComponent } from './componenti/login/login.component';
+import { RegistrazioneComponent } from './componenti/registrazione/registrazione.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    LoginComponent,
+    RegistrazioneComponent
   ],
   imports: [
     BrowserModule,
