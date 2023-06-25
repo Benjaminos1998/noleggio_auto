24ce122539be592114b872185720fbfd53a8022a
Benjaminos1998 <beniamino.infante.98@gmail.com>
2023-06-25 10:53:48 +0200

    Modifiche User Interface
    Aggiunta funzionalità di filtraggio tra le auto. Il filtro consente di selezionare le auto in base alla tipologia auto (Utilitaria-Business-Luxury).

--- Codice/frontend/NoleggioAutoApp/src/app/componenti/auto/auto.component.ts
+++ Codice/frontend/NoleggioAutoApp/src/app/componenti/auto/auto.component.ts
@@ -21,7 +21,10 @@ export class AutoComponent implements OnInit {
   };
   TipologiaAuto: any;
 
-  constructor(private autoService: AutoService) {}
+  public filteredCars?: Auto[];
+
+  constructor(private autoService: AutoService) {
+  }
 
   ngOnInit(): void {
     this.getAllAuto();
@@ -32,7 +35,9 @@ export class AutoComponent implements OnInit {
       .getAllAuto(this.apiServerUrl + '/parcoAuto')
       .subscribe((data) => {
         (this.auto = data), console.log(this.auto);
+        this.filteredCars = this.auto;
       });
+
   }
 
   aggiungiAuto(): void {
@@ -51,28 +56,48 @@ export class AutoComponent implements OnInit {
       });
   }
 
+  //Logica per la gestione del filtro
+
+  onFilterChange(event: Event) {
+    const selectElement = event.target as HTMLSelectElement;
+    let filterValue = selectElement.value;
+    if (filterValue == '1') {
+      this.filteredCars = this.auto;
+    } else if (filterValue == '2') {
+      this.filteredCars = this.auto?.filter(automobile => automobile.tipoAuto.startsWith('U'));
+    } else if (filterValue == '3') {
+      this.filteredCars = this.auto?.filter(automobile => automobile.tipoAuto.startsWith('B'));
+    } else if (filterValue == '4') {
+      this.filteredCars = this.auto?.filter(automobile => automobile.tipoAuto.startsWith('L'));
+    }
+    else if (filterValue == '0') {
+      this.filteredCars = this.auto;
+    }
+  }
+
   // Logica per la gestione dell'immagine
 
   getCarImage(modello: String): string {
-    let imagePath ='';
+    let imagePath = '';
 
-    if(modello == 'Ferrari'){
+    if (modello == 'Ferrari') {
       imagePath = 'assets/images/ferrari.jpg';
-    }else if (modello == 'Bmw' || modello == "BMW"){
-      imagePath = 'https://www.motornet.it/img/modelli/auto/BMW/Serie%208%20Gran%20Coupe_1.jpg';
-    }else if (modello == 'Tesla'){
+    } else if (modello == 'Bmw' || modello == 'BMW') {
+      imagePath =
+        'https://www.motornet.it/img/modelli/auto/BMW/Serie%208%20Gran%20Coupe_1.jpg';
+    } else if (modello == 'Tesla') {
       imagePath = 'assets/images/tesla.png';
-    }else if (modello == 'Fiat'){
-      imagePath = 'https://www.garageromasnc.com/wp-content/uploads/2021/05/Panda.jpg';
-    }else if (modello == 'Alfa Romeo' || modello == 'AlfaRomeo'){
+    } else if (modello == 'Fiat') {
+      imagePath =
+        'https://www.garageromasnc.com/wp-content/uploads/2021/05/Panda.jpg';
+    } else if (modello == 'Alfa Romeo' || modello == 'AlfaRomeo') {
       imagePath = 'assets/images/alfaromeo.png';
     }
 
     return imagePath;
   }
-  /*
-Logica per la gestione del menu
-*/
+
+  //Logica per la gestione del menu
 
   @ViewChild('sidenav') sidenav?: MatSidenav;
   isExpanded = true;
