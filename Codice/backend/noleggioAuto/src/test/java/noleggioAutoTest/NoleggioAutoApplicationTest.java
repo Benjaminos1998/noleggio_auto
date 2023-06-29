0dcb60bbf25dba343dccf060d187ce76a365f399
AlexanderRubino <a.rubino2@studenti.unibg.it>
2023-06-29 23:38:25 +0200

    Creazione test generale app
    Aggiunta dipendenza ai test di springboot in pom.xml + creazione classe di test

---
+++ Codice/backend/noleggioAuto/src/test/java/noleggioAutoTest/NoleggioAutoApplicationTest.java
@@ -0,0 +1,14 @@
+package noleggioAutoTest;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class NoleggioAutoApplicationTest {
+	
+	@Test
+	public void contextLoads() {
+	}
+
+}
