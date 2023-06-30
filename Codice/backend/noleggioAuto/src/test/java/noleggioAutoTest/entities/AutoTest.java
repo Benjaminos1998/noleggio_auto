f3a8e632544625e893b0be8d0c216d4d3a90cbbe
AlexanderRubino <a.rubino2@studenti.unibg.it>
2023-06-30 16:47:49 +0200

    Revisione test entità
    Classici test fatti con Junit

--- Codice/backend/noleggioAuto/src/test/java/noleggioAutoTest/entities/AutoTest.java
+++ Codice/backend/noleggioAuto/src/test/java/noleggioAutoTest/entities/AutoTest.java
@@ -1,38 +1,35 @@
-//package noleggioAutoTest.entities;
-//
-//import static org.junit.Assert.*;
-//
-//import org.junit.Test;
-//
-//import noleggioAuto.entities.Auto;
-//
-//public class AutoTest {
-//
-//	/**
-//	 * Test per il costruttore di Auto senza argomenti
-//	 */
-//	@Test
-//	public void testAuto() {
-//		Auto auto = new Auto();
-//		auto.setTarga("CA123DA");
-//		auto.setModello("Panda");
-//		auto.setId(1);
-//		assertEquals("CA123DA", auto.getTarga());
-//		assertEquals("Panda", auto.getModello());
-//		assertEquals(1, auto.getId(), 0);
-//
-//	}
-//
+package noleggioAutoTest.entities;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import noleggioAuto.entities.Auto;
+
+public class AutoTest {
+
+@Test
+	public void testAuto() {
+		Auto auto = new Auto();
+		auto.setTarga("CA123DA");
+		auto.setModello("Panda");
+		auto.setId(1);
+		assertEquals("CA123DA", auto.getTarga());
+		assertEquals("Panda", auto.getModello());
+		assertEquals(1, auto.getId(), 0);
+
+	}
+
 //	/**
 //	 * Test per il costruttore di Auto con argomenti
 //	 */
-//	@Test
-//	public void testAuto2() {
-//		Auto auto = new Auto(1, "DA123DE", "Bmw");
-//		assertEquals(1, auto.getId(), 0);
-//		assertEquals("DA123DE", auto.getTarga());
-//		assertEquals("Bmw", auto.getModello());
-//	}
+	@Test
+    public void testAuto2() {
+		Auto auto = new Auto(1, "DA123DE", "Bmw");
+		assertEquals(1, auto.getId(), 0);
+ 	    assertEquals("DA123DE", auto.getTarga());
+		assertEquals("Bmw", auto.getModello());
+	}
 //
 //	/**
 //	 * Test per il metodo toString di Auto
